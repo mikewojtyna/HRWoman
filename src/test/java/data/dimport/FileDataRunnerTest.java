eab4b5c5ef83314dab699c637172422220e9643b
Rafał <asd>
2018-06-17 21:01:50 +0200

    Added missing (idk why...) test for FileDataRunner

--- src/test/java/data/dimport/FileDataRunnerTest.java
+++ src/test/java/data/dimport/FileDataRunnerTest.java
@@ -1,7 +1,27 @@
 package data.dimport;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import user.Users;
+
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.*;
 
 class FileDataRunnerTest {
+    @DisplayName("importData: Test null value of file path")
+    @Test
+    void testNullValueInFilePath() {
+        Users usersList = new Users();
+        Importer importer = new FileDataRunner(null);
+        assertThatNullPointerException().isThrownBy(() -> importer.importData(usersList));
+    }
 
+    @DisplayName("importData: Test empty value of file path")
+    @Test
+    void testEmptyValueInFilePath() {
+        Users usersList = new Users();
+        Importer importer = new FileDataRunner(Paths.get("", ""));
+        assertThatIOException().isThrownBy(() -> importer.importData(usersList));
+    }
 }
